f7ff7240f16a382fa159d8735f4afaba14e93c94
Sarath Dontireddy <66492650+sdontireddy@users.noreply.github.com>
2023-02-02 10:00:24 -0500

    Update pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -87,6 +87,18 @@ MENUITEMS = (
              ('AWS', '/category/aws')         
 )
 
+GOOGLE_ADSENSE = {
+    "ca_id": "ca-pub-4811615297206908",
+    "page_level_ads": True,
+    'ads': {
+        'aside': '1234561',          # Side bar banner (all pages)
+        'main_menu': '1234562',      # Banner before main menu (all pages)
+        'index_top': '1234563',      # Banner after main menu (index only)
+        'index_bottom': '1234564',   # Banner before footer (index only)
+        'article_top': '1234565',    # Banner after article title (article only)
+        'article_bottom': '1234566', # Banner after article content (article only)
+    }
+}
 
 SITEMAP = {
     'format': 'xml',
